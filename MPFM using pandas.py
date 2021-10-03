e493a2c3d02d460b9a9c3d8cce55884e62fb7c6a
wcqrwtqr <mohalbatati@hotmail.com>
2021-10-04 00:30:21 +0300

    Updated the code and added more comments to get the code more readable and human freindly, also updated the readme file

--- MPFM using pandas.py
+++ MPFM using pandas.py
@@ -9,28 +9,50 @@ Created on Sat Oct 21 21:42:30 2017
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
 
 #=========================================================================
 #================================ PARAMETERS =============================
 n = 300
-m = 1100
+m = 800
 N = 5
 #============================== IMPORTING ================================
-file= '/Users/mohammedalbatati/AnacondaProjects/OilSERV/Reading csv using pandas/MPFM-python/ZB-10second.log'
+'''
+Using the os library this help getting the folder full path and putting the file inside the folder makes it easy to mention the name of the file only
+Below code is to import the MPFM log files to the pandas dataframe 
+'''
+package_dir = os.path.dirname(os.path.abspath(__file__))
+# the file full path is appended with the file name only
+file = os.path.join(package_dir,'335.log')
+# Using the sep='\t' as the dimilter since the file is a tab file
 MPFM_data = pd.read_csv(file,sep='\t')
-#MPFM_data = pd.read_csv('ZB-10second.log',sep='\t')
+# print(MPFM_data.columns)
 
 #========================== REMOVING BAD COLUMNS & HEADERS ===============
-#MPFM_data.drop('Unnamed: 31', axis = 1, inplace = True)
-MPFM_data.drop(MPFM_data[[18,19,20,21,22,23,24,25,26,27,28,29,30,31]],axis=1, inplace = True)
-MPFM_data.drop('Unnamed: 32', axis = 1, inplace = True)
+'''
+Using the try except to remove unwanted columns from the data frame, if is needed then the below code can be removed
+'''
+try:
+    MPFM_data.drop(['Permittivity', 'Conductivity', 'Act.OilMassRate', 'Act.WaterMassRate', 'Act.GasMassRate', 'Std.OilMassRate', 'Std.WaterMassRate', 'Std.GasMassRate', 'Std.AccumOilVol', 'AccumWaterVol', 'Std.AccumGasVol', 'Std.AccumGasVol.1', 'Unnamed: 31'], axis = 1, inplace = True)
+except:
+    pass
+
+# print(MPFM_data.columns)
 
 
 
 #========================= CONVERTING TIME AND DATE ======================
+'''
+Combining the two strings of date and time togather with space then using the funciton to_datetime to convert to a date time instant
+'''
 MPFM_data['comb_datetime'] = pd.to_datetime(MPFM_data['Date']+' '+MPFM_data['Clock'])
-MPFM_data['new_Time'] = MPFM_data['comb_datetime'].dt.time
+# MPFM_data['new_Time'] = MPFM_data['Clock'].dt.time
+# print(MPFM_data)
+# print(MPFM_data.describe())
+# print(MPFM_data.info())
 
+'''
 #trying to make the comulative column ////// still trying
 #MPFM_data['oil_cum'] = MPFM_data['Std.OilFlowrate'] + MPFM_data['Std.OilFlowrate'].shift(0)
 #for i in range(0, len(MPFM_data)):
@@ -41,32 +63,35 @@ MPFM_data['new_Time'] = MPFM_data['comb_datetime'].dt.time
 
 #print(MPFM_data['Std.OilFlowrate'])
 #print(MPFM_data.columns)
+'''
 
 #============================ SUMMARY OF RESULTS ==========================
-avg_P =         np.average(MPFM_data.loc[n:m,'Pressure'])
-avg_T =         np.average(MPFM_data.loc[n:m,'Temperature'])
-avg_dP =        np.average(MPFM_data.loc[n:m,'dP'])
-avg_oilRate =   np.average(MPFM_data.loc[n:m,'Std.OilFlowrate'])
-avg_waterRate = np.average(MPFM_data.loc[n:m,'WaterFlowrate'])
-avg_std_gasRate=np.average(MPFM_data.loc[n:m,'Std.GasFlowrate'])
-avg_act_gasRate=np.average(MPFM_data.loc[n:m,'Act.GasFlowrate'])
-avg_GOR =       np.average(MPFM_data.loc[n:m,'GOR(std)'])
-avg_WC =        np.average(MPFM_data.loc[n:m,'Std.Watercut'])
-avg_oilSG =     np.average(MPFM_data.loc[n:m,'OilDensity'])
-avg_waterSG =   np.average(MPFM_data.loc[n:m,'WaterDensity'])
-avg_gasSG =     np.average(MPFM_data.loc[n:m,'GasDensity'])
-avg_dP =        np.average(MPFM_data.loc[n:m,'dP'])
-first_time =    MPFM_data['Date'].iget(0) +' '+ MPFM_data.loc[n:m,'Clock'].iget(0)
-last_time =     MPFM_data['Date'].iget(0) +' '+ MPFM_data.loc[n:m,'Clock'].iget(-1)
-first_time =    pd.to_datetime(first_time)
-last_time =     pd.to_datetime(last_time)
-avg_liquid =    avg_oilRate + avg_waterRate
-API =           (141.5/(avg_oilSG/1000) - 131.5)
-#oil_cum =       MPFM_data.loc[n:m,'Std.OilFlowrate'].cumsum()
-
-
+'''
+Below is generating the averages for all the values needed for the report
+'''
+avg_P           = np.average(MPFM_data.loc[n:m,'Pressure'])
+avg_T           = np.average(MPFM_data.loc[n:m,'Temperature'])
+avg_dP          = np.average(MPFM_data.loc[n:m,'dP'])
+avg_oilRate     = np.average(MPFM_data.loc[n:m,'Std.OilFlowrate'])
+avg_waterRate   = np.average(MPFM_data.loc[n:m,'WaterFlowrate'])
+avg_std_gasRate = np.average(MPFM_data.loc[n:m,'Std.GasFlowrate'])
+avg_act_gasRate = np.average(MPFM_data.loc[n:m,'Act.GasFlowrate'])
+avg_GOR         = np.average(MPFM_data.loc[n:m,'GOR(std)'])
+avg_WC          = np.average(MPFM_data.loc[n:m,'Std.Watercut'])
+avg_oilSG       = np.average(MPFM_data.loc[n:m,'OilDensity'])
+avg_waterSG     = np.average(MPFM_data.loc[n:m,'WaterDensity'])
+avg_gasSG       = np.average(MPFM_data.loc[n:m,'GasDensity'])
+avg_dP          = np.average(MPFM_data.loc[n:m,'dP'])
+first_time      = MPFM_data['comb_datetime'].iloc[1]
+last_time       = MPFM_data['comb_datetime'].iloc[-1]
+avg_liquid      = avg_oilRate + avg_waterRate
+API             = (141.5/(avg_oilSG/1000) - 131.5)
+# oil_cum         = MPFM_data.loc[n:m,'Std.OilFlowrate'].cumsum()
 
 #============================== SUMMARY TO DATAFRAME=======================
+'''
+Add all the values needed for the report to a dict before moving it to a dataframe
+'''
 my_summary = {'From':first_time,
               'To':last_time,
               'Delta time':'??????',
@@ -91,16 +116,18 @@ my_summary = {'From':first_time,
 
 summary = pd.DataFrame([my_summary]) # convert the dict to dataframe ;)
 
+'''
+This is an optional code and might not be needed
 #========================== DATA REDUCING ==========================
 
 #test_data = MPFM_data.groupby(np.arange(len(MPFM_data))//5).mean()
 #test_data2 = MPFM_data.loc[::5,:]
+'''
 
-#============================= GRAPHING ==================================
 
-x = MPFM_data.loc[n:m,'new_Time']
+x = MPFM_data.loc[n:m,'comb_datetime']
 
-plt.figure()
+plt.figure('Pressure vs dP vs Temp')
 plt.plot(x,MPFM_data.loc[n:m,'Pressure'],'b-')
 plt.plot(x,MPFM_data.loc[n:m,'dP'],'g-')
 plt.plot(x,MPFM_data.loc[n:m,'Temperature'],'r-')
@@ -109,24 +136,23 @@ plt.xticks(rotation=45)
 # Pad margins so that markers don't get clipped by the axes
 plt.margins(0.05)
 plt.subplots_adjust(bottom=0.15)
-plt.legend()
-plt.show
+plt.legend('PDT')
+# plt.show
 
-plt.figure()
+
+plt.figure('Flow rate')
 plt.plot(x,MPFM_data.loc[n:m,'Std.OilFlowrate'],'k-')
 plt.plot(x,MPFM_data.loc[n:m,'GOR(std)'],'g-')
 plt.plot(x,MPFM_data.loc[n:m,'WaterFlowrate'],'b-')
 plt.xticks(rotation=45)
-plt.legend()
-plt.show
+plt.legend('Test')
 
-plt.figure()
+plt.figure('Gas rate and water cut')
 plt.plot(x,MPFM_data.loc[n:m,'Std.GasFlowrate'],'y-')
 plt.plot(x,MPFM_data.loc[n:m,'Std.Watercut'],'b-')
 plt.xticks(rotation=45)
 plt.legend()
-plt.show
-
+plt.show()
 
 
 #=============================  EXPORTING SUMMARY TO CSV ===================================
@@ -138,16 +164,22 @@ plt.show
 
 #============================= EXPORTING TO EXCEL =========================================
 
-writer = pd.ExcelWriter('output.xlsx')
+'''
+The code below is to use the excel writer to output an excel sheet having the data in one sheet and the summary of data (averages) in the other sheet
+'''
+try:
+    writer = pd.ExcelWriter('output.xlsx')
+    MPFM_data.loc[n:m:N,['Date', 'Clock', 'Pressure', 'Temperature', 'dP', 'Std.OilFlowrate',
+                         'WaterFlowrate', 'Std.GasFlowrate', 'Act.GasFlowrate', 'GOR(std)',
+                         'Act.OilFlowrate', 'Std.Watercut','OilDensity', 'WaterDensity',
+                         'GasDensity']].to_excel(writer,'sheet1')
 
-MPFM_data.loc[n:m:N,['Date', 'Clock', 'Pressure', 'Temperature', 'dP', 'Std.OilFlowrate',
-       'WaterFlowrate', 'Std.GasFlowrate', 'Act.GasFlowrate', 'GOR(std)',
-       'Act.OilFlowrate', 'Std.Watercut','OilDensity', 'WaterDensity', 
-       'GasDensity']].to_excel(writer,'sheet1')
+    summary.to_excel(writer,'sheet2')
 
-summary.to_excel(writer,'sheet2')
+    writer.save()
 
-writer.save()
+except:
+    print('Could not export the data to excel')
+    pass
 
-# checking
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MPFM-python
+The script was made to take the .log files from the MPFM unit of roxar meters and add it to the dataframe of pandas then perform some computation on it and generate a graph to ensure that the data needed for the summary are stable or not.
+If the data are not stable then its advised to change the start point n and end point m to get a stable period of reading so the average values are as best as possible.
